200e999db40653ccba21b16549ea8b4d5ce78802
BujarMurati <39311213+BujarMurati@users.noreply.github.com>
2019-11-02 16:26:12 +0100

    set up project with material and flex layout

---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import {
+    MatButtonModule
+} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports:[
+        MatButtonModule,
+    ],
+    exports:[
+        MatButtonModule,
+    ],
+})
+export class MaterialModule{
+
+}
