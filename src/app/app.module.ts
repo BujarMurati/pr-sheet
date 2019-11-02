cd88d37bb31918c50a83f876ceb5ffd297b98134
BujarMurati <39311213+BujarMurati@users.noreply.github.com>
2019-11-02 16:30:46 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LiftComponent } from './lift/lift.component';
+import { BestComponent } from './lift/best/best.component';
+import { HistoryComponent } from './lift/history/history.component';
+import { TrackComponent } from './lift/track/track.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavigationComponent,
+    DashboardComponent,
+    LiftComponent,
+    BestComponent,
+    HistoryComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule,
