72b5adb543fd1e0b28358eb279c3bb0dcfc1a7ce
BujarMurati <39311213+BujarMurati@users.noreply.github.com>
2019-11-02 16:34:37 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LiftComponent } from './lift/lift.component';
+import { BestComponent } from './lift/best/best.component';
+import { HistoryComponent } from './lift/history/history.component';
+import { TrackComponent } from './lift/track/track.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: ':lift', component: LiftComponent},
+  {path: ':lift/best', component: BestComponent},
+  {path: ':lift/history', component: HistoryComponent},
+  {path: ':lift/track', component: TrackComponent},
+  {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
