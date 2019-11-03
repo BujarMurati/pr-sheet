65eeff79533318f2c7b7f4abf3d4092ff0551967
BujarMurati <39311213+BujarMurati@users.noreply.github.com>
2019-11-03 22:57:49 +0100

    finished dashboard table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PrService } from '../pr.service';
+import { PR, PRCollection } from '../pr';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  bestPRs: PR[];
+
+  constructor(
+    private prService: PrService,
+  ) { }
 
   ngOnInit() {
+    this.prService.read()
+      .then(prs =>{
+        this.bestPRs = (new PRCollection(prs)).getBestPRsBy('lift');
+      })
+      .catch(err => console.error(err));
   }
 
 }
