0d86940ee0a342c6f024ff90f567bacb89de5901
BujarMurati <39311213+BujarMurati@users.noreply.github.com>
2019-11-07 20:15:16 +0100

    added history component

--- src/app/lift/history/history.component.ts
+++ src/app/lift/history/history.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PR, PRCollection } from 'src/app/pr';
+import { PrService } from 'src/app/pr.service';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.css']
 })
 export class HistoryComponent implements OnInit {
+  @Input() 
+  set lift(lift: string){
+    this._lift = lift;
+    this.getAllPRs();
+  }
+  get name(): string {return this._lift}
 
-  constructor() { }
+  private _lift: string;
+
+  allPRs: PR[];
+
+  constructor(
+    private prService: PrService,
+  ) { }
 
   ngOnInit() {
+    this.getAllPRs();
+  }
+
+  getAllPRs(): void{
+    this.prService.read({lift:this._lift})
+    .then(prs => {
+      this.allPRs = prs;
+      this.allPRs.sort(function(a, b){
+        if (a.date > b.date){
+          return 1;
+        }else if (a.date === b.date){
+          return 0;
+        }else{
+          return -1;
+        }
+      });
+      
+    })
+    .catch(err => console.error(err));
+
   }
 
 }
